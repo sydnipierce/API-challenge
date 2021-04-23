27b3e4cf96fa96f9b0479e197060d1e42f80a977
Sydni <sydnimpierce@gmail.com>
2021-04-23 11:59:42 -0400

    Added API keys, commented out citipy library as unable to install in Conda environment

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "e1406094bd9b1565fa30a622c3d86626"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAH3VIMyWjWguHWZuF7wBmHIA-3YONnQ_4"
